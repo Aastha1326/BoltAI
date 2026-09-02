cafed51dfaa5f46788997591eacb37018219e923
Aastha Dua <aastha.dua2006@gmail.com>
2026-09-02 09:24:32 +0530

    chnaged gemini model

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 // ============================================================
 
 const model = new ChatGoogleGenerativeAI({
-  model: "gemini-2.5-flash",
+  model: "gemini-3.5-flash",
   temperature: 0.2,
   apiKey: process.env.apikey
 });
